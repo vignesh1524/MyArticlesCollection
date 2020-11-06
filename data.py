172f936e2067f66b376808a704ae29c5fb202c12
vignesh1524 <43825542+vignesh1524@users.noreply.github.com>
2020-11-06 16:47:07 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Article-1',
+            'Body' : 'Hello world for the articles are written here',
+            'Author' : 'Author-1',
+            'Created-date' : '10-10-2020'
+        },
+        {
+            'id' : 2,
+            'title' : 'Article-2',
+            'Body' : 'Hello world for the articles are written here',
+            'Author' : 'Author-2',
+            'Created-date' : '10-10-2020'
+        },
+        {
+            'id' : 3,
+            'title' : 'Article-3',
+            'Body' : 'Hello world for the articles are written here',
+            'Author' : 'Author-3',
+            'Created-date' : '10-10-2020'
+        }
+    ]
+    return articles
